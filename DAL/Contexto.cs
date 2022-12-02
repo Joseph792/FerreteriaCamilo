533ec18eb10a9ed047a0d392bfd80b6c780f8429
Joseph792 <97407858+Joseph792@users.noreply.github.com>
2022-12-02 01:50:08 -0400

    Agregando la Base de Datos

--- DAL/Contexto.cs
+++ DAL/Contexto.cs
@@ -6,7 +6,6 @@ namespace FerreteriaCamilo.DAL
     public class Contexto : DbContext
     {
     public DbSet <Clientes> Clientes { get; set; }
-
     public Contexto(DbContextOptions<Contexto> options): base(options)
     {
     }
