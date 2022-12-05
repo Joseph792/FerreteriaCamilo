0e1a7b2a8aeda21c95b1a64c569ae996aafafe21
Joseph792 <97407858+Joseph792@users.noreply.github.com>
2022-12-05 01:44:41 -0400

    Agregando Articulos al contexto

--- DAL/Contexto.cs
+++ DAL/Contexto.cs
@@ -3,6 +3,7 @@
     public class Contexto : DbContext
     {
     public DbSet <Clientes> Clientes { get; set; }
+    public DbSet <Articulos> Articulos { get; set; }
     public Contexto(DbContextOptions<Contexto> options): base(options)
     {
     }
