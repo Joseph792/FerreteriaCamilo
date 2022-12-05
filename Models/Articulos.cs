baa09ea11ae4bc4f0d91c88b7bbe536c18b91f0f
Joseph792 <97407858+Joseph792@users.noreply.github.com>
2022-12-05 01:35:08 -0400

    Modificando entidad Articulos

--- Models/Articulos.cs
+++ Models/Articulos.cs
@@ -1,9 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
 public class Articulos
 {
+[Key]
 public int ArticuloId { get; set; }
+[Required(ErrorMessage ="El nombre es obligatorio")]
 public String? Nombre { get; set; }
+[Required(ErrorMessage ="La referencia es obligatoria")]
 public String? Referencia { get; set; }
+[Range(minimum:0, maximum:5000000, ErrorMessage ="El costo es obligatorio")]
 public int Costo { get; set; }
+[Range(minimum:0, maximum:5000000, ErrorMessage ="El costo es obligatorio")]
 public int Precio { get; set; }
-public String? Suplidor { get; set; }
+public String? SuplidorId { get; set; }
 }
