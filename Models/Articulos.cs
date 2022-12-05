ec1ba3ff7f2d28574e02460cdb057f8914f42211
Joseph792 <97407858+Joseph792@users.noreply.github.com>
2022-12-05 01:29:17 -0400

    Agregando Entidad Articulos

---
+++ Models/Articulos.cs
@@ -0,0 +1,9 @@
+public class Articulos
+{
+public int ArticuloId { get; set; }
+public String? Nombre { get; set; }
+public String? Referencia { get; set; }
+public int Costo { get; set; }
+public int Precio { get; set; }
+public String? Suplidor { get; set; }
+}
